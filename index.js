38d3945358e7c5e0ec213cc553859037cd9a2847
DInnaD <DInnaD@users.noreply.github.com>
2020-01-11 20:08:46 +0200

    Update index.js

--- index.js
+++ index.js
@@ -4,7 +4,6 @@ $(function(){
 // https://cdnjs.cloudflare.com/ajax/libs/instafeed.js/1.4.1/instafeed.min.js
 // https://matthewelsom.com/assets/js/libs/instafeed.min.js
 
-
   //5679701317 5679701317.8f4c5bf.69b3f2c784fe48df9aa9912635f1ffe0
   //https://api.instagram.com/v1/users/self/media/liked?access_token=ACCESS-TOKEN
 */
